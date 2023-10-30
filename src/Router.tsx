4f15d59171581c5ff36491e4caf766a261fa3dd3
Sang Hoon Kim <KSHoon522@gamil.com>
2023-10-30 16:59:18 +0900

    ErrorComponent

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import About from "./screens/About";
 import Home from "./screens/Home";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./component/ErrorComponent";
 
 const router = createBrowserRouter([
   //Router를 array 형태로 생성하게 해줌
@@ -12,12 +14,15 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+        errorElement: <ErrorComponent />, //Home component가 crash했을 때 보여주는 element
+        //다른 컴포넌트를 Home 컴포넌트에서 발생하는 문제로부터 보호해줌
       },
       {
         path: "about",
         element: <About />,
       },
     ],
+    errorElement: <NotFound />, //에러가 발생했거나 컴포넌트를 못 찾을 때 보여줄 element
   },
 ]);
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,6 @@
 function Home() {
-  return <h1>Home</h1>;
+  const users: any = [];
+  return <h1>{users[0].name}</h1>; //일부러 에러를 발생시킴
 }
 
 export default Home;
