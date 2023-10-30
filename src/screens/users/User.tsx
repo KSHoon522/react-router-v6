a6ddf4c79310243a9cab25f27361078c77bae570
Sang Hoon Kim <KSHoon522@gamil.com>
2023-10-30 17:51:32 +0900

    useOutletContext

--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -11,7 +11,12 @@ function User() {
       <hr />
       <Link to="followers">See followers</Link>{" "}
       {/*followers를 사용한다면 절대 경로인 locathost:3000/followers에 가게 된다 -> /가 없는 상대 경로로 작성*/}
-      <Outlet />
+      <Outlet
+        context={{
+          nameOfMyUser: users[Number(userId) - 1].name,
+        }}
+      />
+      {/* User 컴포넌트가 render하고 있는 자식들에게 context의 값을 전달하고 있음 */}
     </div>
   );
 }
