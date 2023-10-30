c2a6ce3acd39dbed8f51b0e4c84e5659231d25a1
Sang Hoon Kim <KSHoon522@gamil.com>
2023-10-30 17:44:48 +0900

    Outlet

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import Home from "./screens/Home";
 import NotFound from "./screens/NotFound";
 import ErrorComponent from "./component/ErrorComponent";
 import User from "./screens/users/User";
+import Followers from "./screens/users/Followers";
 
 const router = createBrowserRouter([
   //Router를 array 형태로 생성하게 해줌
@@ -26,6 +27,12 @@ const router = createBrowserRouter([
         path: "users/:userId", // ./users에 아무것도 보여줄 것이 없으면 이런 식으로 작성
         //아니라면 users 밑에 children을 추가해줘야함
         element: <User />,
+        children: [
+          {
+            path: "followers",
+            element: <Followers />,
+          },
+        ],
       },
     ],
     errorElement: <NotFound />, //에러가 발생했거나 컴포넌트를 못 찾을 때 보여줄 element
--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -1,12 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, Outlet, Link } from "react-router-dom";
 import { users } from "../../db";
 
 function User() {
   const { userId } = useParams();
   return (
-    <h1>
-      User with it {userId} is named : {users[Number(userId) - 1].name}
-    </h1>
+    <div>
+      <h1>
+        User with it {userId} is named : {users[Number(userId) - 1].name}
+      </h1>
+      <hr />
+      <Link to="followers">See followers</Link>{" "}
+      {/*followers를 사용한다면 절대 경로인 locathost:3000/followers에 가게 된다 -> /가 없는 상대 경로로 작성*/}
+      <Outlet />
+    </div>
   );
 }
 export default User;
